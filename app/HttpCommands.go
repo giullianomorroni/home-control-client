052686e6333e707c16b6fe5328f44fab12173473
giullianomorroni <giullianomorroni@gmail.com>
2015-01-27 22:40:36 -0200

    troca url

--- app/HttpCommands.go
+++ app/HttpCommands.go
@@ -13,13 +13,13 @@ func init() {
 }
 
 func LookUpUpdates(account string) (string) {
-	//response, err := http.Get("http://controlinside.com.br/comandos?account="+account)
-	response, err := http.Get("http://localhost:9000/comandos?account="+account)
+	response, err := http.Get("http://controlinside.com.br/comandos/"+account)
+	//response, err := http.Get("http://localhost:9000/comandos/"+account)
+	defer response.Body.Close()
     command := "";
     if err != nil {
         fmt.Println(err)
     } else {
-        defer response.Body.Close()
         contents, err := ioutil.ReadAll(response.Body)
         if err != nil {
             fmt.Println(err)
